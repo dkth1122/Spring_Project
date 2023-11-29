3c8eb6d381a47266716c7c44935e13c8f00dd765
dkth1122 <134511884+dkth1122@users.noreply.github.com>
2023-11-30 04:13:14 +0900

    Delete Spring_Project4 directory

--- Spring_Project4/src/main/java/com/example/project/dao/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.dao;
-
-import java.util.List;
-
-import com.example.project.model.Test;
-
-public interface TestService {
-	List<Test> searchTest();
-}
--- Spring_Project4/src/main/java/com/example/project/model/Test.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.model;
-
-import lombok.Data;
-
-@Data
-public class Test {
-	private String test;
-}
